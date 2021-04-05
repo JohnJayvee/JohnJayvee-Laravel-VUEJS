bf7815c7c7d7e309a98804625fbf9cf68d6a05e9
JohnJayvee <johnjayveesiuagan@gmail.com>
2021-04-05 23:37:35 +0800

    FINAL

--- app/Http/Controllers/UpdateProfileController.php
+++ app/Http/Controllers/UpdateProfileController.php
@@ -55,6 +55,8 @@ class UpdateProfileController extends Controller
 
         ]);
         // dd('Password change successfully.');
-        return redirect()->back()->with('message', '✔ Your account has been updated');
+        // return redirect()->back()->with('message', '✔ Your account has been updated');
+            return redirect()->back()->withErrors(['message' => 'These credentials do not match our records']);
+
     }
 }
